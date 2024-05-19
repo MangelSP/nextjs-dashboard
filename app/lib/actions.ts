6fc6170380ff4a76fea99cb2149f8b7f8ac34bca
Miguel Angel Sanchez Peralta <yacalos_@hotmail.com>
2024-05-18 23:04:11 -0400

    removed code shit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,16 +8,6 @@ import { signIn } from "@/auth";
 import { AuthError } from 'next-auth';
 
 
-export type FormState =
-| {
-    errors?: {
-      name?: string[]
-      email?: string[]
-      password?: string[]
-    }
-    message?: string
-  }
-| undefined
 
 
 export type State = {
@@ -41,22 +31,7 @@ export type State = {
     date: z.string(),
 });
 
-export const SignupFormSchema = z.object({
-  name: z
-    .string()
-    .min(2, { message: 'Name must be at least 2 characters long.' })
-    .trim(),
-  email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
-  password: z
-    .string()
-    .min(8, { message: 'Be at least 8 characters long' })
-    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
-    .regex(/[0-9]/, { message: 'Contain at least one number.' })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: 'Contain at least one special character.',
-    })
-    .trim(),
-})
+
 
 const CreateInvoice = FormSchema.omit({id:true,date:true})
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
